f0d504546b477c2ee304c740967ddf27c3c29f9b
LoveAI-Network <132933966+DisciplesofLove@users.noreply.github.com>
2025-07-05 20:28:06 +0300

    cryptonote_basic: document async inference routing

--- src/cryptonote_basic/inference_router.cpp
+++ src/cryptonote_basic/inference_router.cpp
@@ -1,6 +1,7 @@
 #include "inference_router.h"
 
 namespace cryptonote {
+  // TODO: send results asynchronously once networking code is ready
   void route_inference(const std::string &serialized_result)
   {
     // In a real deployment, this would forward inference results
